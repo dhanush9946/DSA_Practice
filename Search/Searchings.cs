d8cc3758ff293b5b25df78839ee820d4e1caf2ba
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-02-09 16:06:57 +0530

    Binary search added

--- Search/Searchings.cs
+++ Search/Searchings.cs
@@ -10,8 +10,11 @@ namespace DSA.Search
     {
         public static void search()
         {
-            int a = DifferentSearches.LinearSerch(new[] { 2, 4, 6, 9, 4, 7 }, 9);
-            Console.WriteLine("The Element is find at the index number:"+ a);
+            //int a = DifferentSearches.LinearSerch(new[] { 2, 4, 6, 9, 4, 7 }, 9);
+            //Console.WriteLine("The Element is find at the index number:"+ a);
+
+            int a = DifferentSearches.BinarySearch(new int[] { 1,2,3,4,5,6,7,8 },7);
+            Console.WriteLine("Index of the Target:"+a);
         }
     }
     class DifferentSearches
@@ -25,6 +28,32 @@ namespace DSA.Search
                 {
                     return i;
                 }
+            }
+            return -1;
+        }
+
+        public static int BinarySearch(int[] arr,int target)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+
+                else if (arr[low] < target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+
             }
             return -1;
         }
